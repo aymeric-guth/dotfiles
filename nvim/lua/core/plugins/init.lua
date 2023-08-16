1f5475a88dd8c1b74a773088d037e76aba45fd25
yul <aymeric.guth@protonmail.com>
2023-08-16 09:00:07 +0200

    Add gitsigns plugin

--- nvim/lua/core/plugins/init.lua
+++ nvim/lua/core/plugins/init.lua
@@ -29,14 +29,23 @@ return {
       },
     },
   },
-
-  -- {
-  --   'lukas-reineke/indent-blankline.nvim',
-  --   opts = {
-  --     char = '┊',
-  --     show_trailing_blankline_indent = false,
-  --   },
-  -- },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+    },
+    -- config = function()
+    --     vim.opt.list = true
+    --     vim.opt.listchars:append "space:⋅"
+    --     vim.opt.listchars:append "eol:↴"
+    --     require("indent_blankline").setup({
+    --         show_end_of_line = true,
+    --         space_char_blankline = " ",
+    --     })
+    -- end,
+  },
   require 'core.plugins.telescope',
   require 'core.plugins.treesitter',
+  require 'core.plugins.gitsigns',
 }
