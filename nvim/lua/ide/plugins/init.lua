57b13a04beac298c4272e63f7c78d04405e9f02f
yul <aymeric.guth@protonmail.com>
2023-08-18 07:41:02 +0200

    Add conditional loading of LSP

--- nvim/lua/ide/plugins/init.lua
+++ nvim/lua/ide/plugins/init.lua
@@ -5,5 +5,12 @@ return {
   require('ide.plugins.illuminate'),
   require('ide.plugins.nvim-colorizer'),
   require('ide.plugins.goto-preview'),
-  require('ide.plugins.lsp-zero'),
+  (function() 
+      if os.getenv("WORKSPACE") ~= nil then 
+          return require('ide.plugins.lsp-zero')
+      else 
+          print("WORKSPACE is not defined")
+          return {} 
+      end 
+  end)()
 }
