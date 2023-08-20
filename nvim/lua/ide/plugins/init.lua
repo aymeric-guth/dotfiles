c606ea257e3cc023ae590a756f9af220b9fba9bc
yul <aymeric.guth@protonmail.com>
2023-08-20 08:49:20 +0200

    Update lsp activation function

--- nvim/lua/ide/plugins/init.lua
+++ nvim/lua/ide/plugins/init.lua
@@ -1,16 +1,17 @@
 return {
+  'renerocksai/telekasten.nvim',
   require('core.plugins'),
   require('ide.plugins.nvim-tree'),
   require('ide.plugins.harpoon'),
   require('ide.plugins.illuminate'),
   require('ide.plugins.nvim-colorizer'),
   require('ide.plugins.goto-preview'),
-  (function() 
-      if os.getenv("WORKSPACE") ~= nil then 
-          return require('ide.plugins.lsp-zero')
-      else 
-          print("WORKSPACE is not defined")
-          return {} 
-      end 
-  end)()
+  (function()
+    if os.getenv('WORKSPACE') ~= nil then
+      return require('ide.plugins.lsp-zero')
+    else
+      print('WORKSPACE is not defined')
+      return {}
+    end
+  end)(),
 }
