3d484e321eabee5c3fc60fa63a0fbe7bc43e679a
yul <aymeric.guth@protonmail.com>
2023-11-06 17:56:57 +0100

    Add oil plugin

--- nvim/lua/ide/plugins/init.lua
+++ nvim/lua/ide/plugins/init.lua
@@ -8,6 +8,7 @@ return {
   require('ide.plugins.goto-preview'),
   require('ide.plugins.luasnip'),
   require('ide.plugins.trouble'),
+  require('ide.plugins.oil'),
 
   (function()
     if os.getenv('WORKSPACE') ~= nil then
