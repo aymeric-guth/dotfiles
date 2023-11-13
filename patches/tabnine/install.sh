8b250c4f36ee234752d27f6ad99d0997f9a9df88
yul <aymeric.guth@protonmail.com>
2023-11-13 20:20:10 +0100

    Clean, removed unused

--- patches/tabnine/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SRC="$DOTFILES/patches/tabnine/source.lua"
-DST="$HOME/.local/share/nvim/site/pack/packer/start/cmp-tabnine"
-cp "$SRC" "$DST/lua/cmp_tabnine/source.lua" || return 1
-"$DST/install.sh"
-chmod +x -R "$DST/binaries"
