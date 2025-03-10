abd2e4cfb5d87ae624e0af565ecee7c84f35d186
yul <aymeric.guth@protonmail.com>
2025-03-10 18:53:23 +0100

    Fix display name in remap script

--- remaps/setup.monitor.sh
+++ remaps/setup.monitor.sh
@@ -21,8 +21,8 @@ else
 	rm -f "$HOME"/.config/alacritty/alacritty.toml
 	ln -rs "$DOTFILES"/alacritty/alacritty-linux.toml "$HOME"/.config/alacritty/alacritty.toml
 	xrandr \
-		--output eDP-1-1 --primary --mode 1920x1080 --rate 144.00*+ --pos 0x0 --rotate normal --scale 0.75x0.75 \
-		--output HDMI-1-1 --off \
-		--output DP-1-1 --off \
-		--output HDMI-1-2 --off
+		--output eDP-1 --primary --mode 1920x1080 --rate 144.00*+ --pos 0x0 --rotate normal --scale 0.75x0.75 \
+		--output HDMI-1 --off \
+		--output DP-1 --off \
+		--output HDMI-1 --off
 fi
