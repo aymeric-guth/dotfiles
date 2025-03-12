77bf29b42d4c5915e21e8de5c4de8529d88c46da
yul <aymeric.guth@protonmail.com>
2025-03-12 20:29:36 +0100

    Remove allacrity distiction for laptop/clamshell

--- remaps/setup.monitor.sh
+++ remaps/setup.monitor.sh
@@ -9,17 +9,19 @@ setxkbmap -layout custom
 
 if /usr/bin/xrandr | grep '^DP-1 connected' >/dev/null 2>&1; then
 	echo "clamshell"
-	rm -f /home/yul/.config/alacritty/alacritty.toml
-	ln -rs "$DOTFILES"/alacritty/alacritty-linux-clamshell.toml "$HOME"/.config/alacritty/alacritty.toml
+	rm -f "$HOME"/.Xresources
+	ln -rs "$DOTFILES"/remaps/.Xresources.clamshell "$HOME"/.Xresources
+	xrdb -merge "$HOME"/.Xresources
 	xrandr \
 		--output eDP-1 --off \
 		--output HDMI-1 --off \
-		--output DP-1 --primary --mode 3840x2160 --rate 165.00 --pos 0x0 --rotate normal \
+		--output DP-1 --primary --mode 3840x2160 --rate 165.00 --pos 0x0 --dpi 220 --rotate normal \
 		--output HDMI-1 --off
 else
 	echo "laptop"
-	rm -f "$HOME"/.config/alacritty/alacritty.toml
-	ln -rs "$DOTFILES"/alacritty/alacritty-linux.toml "$HOME"/.config/alacritty/alacritty.toml
+	rm -f "$HOME"/.Xresources
+	ln -rs "$DOTFILES"/remaps/.Xresources.laptop "$HOME"/.Xresources
+	xrdb -merge "$HOME"/.Xresources
 	xrandr \
 		--output eDP-1 --primary --mode 1920x1080 --rate 144.00*+ --pos 0x0 --rotate normal --scale 0.75x0.75 \
 		--output HDMI-1 --off \
