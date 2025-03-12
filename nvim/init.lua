8fa5354603c009b93936ae36295ec6bfdfaec9cc
yul <aymeric.guth@protonmail.com>
2025-03-12 20:43:06 +0100

    Remove core layer

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,23 +5,19 @@ end
 
 local layer = os.getenv('AV_NVIM_LAYER')
 
-if layer == 'core' or layer == 'ide' then
-  require(layer)
-  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable', -- latest stable release
-      lazypath,
-    })
-  end
-  vim.opt.rtp:prepend(lazypath)
-  require('lazy').setup({
-    require(layer .. '.plugins'),
-  }, {})
-else
-  error('Invalid layer: ' .. layer)
+require('ide')
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup({
+  require(layer .. '.plugins'),
+}, {})
