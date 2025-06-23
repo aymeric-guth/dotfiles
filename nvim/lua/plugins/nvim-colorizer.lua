8cb8e65ed93b8f2175d6e61b044fc6f4433e4293
yul <aymeric.guth@protonmail.com>
2025-06-23 21:40:15 +0200

    Remove color for words in nvim

--- nvim/lua/plugins/nvim-colorizer.lua
+++ nvim/lua/plugins/nvim-colorizer.lua
@@ -1,11 +1,12 @@
 return {
   'norcalli/nvim-colorizer.lua',
   config = function()
-    vim.cmd([[
-      augroup ColorizeBuffer
-      autocmd!
-      au BufRead,BufNewFile * ColorizerAttachToBuffer
-      augroup END
-    ]])
+    -- vim.cmd([[
+    --   augroup ColorizeBuffer
+    --   autocmd!
+    --   au BufRead,BufNewFile * ColorizerAttachToBuffer
+    --   augroup END
+    -- ]])
+    require('colorizer').setup({}, { mode = 'foreground', names = false })
   end,
 }
