0920279543bc75012d7a785c8dc1e83de454b866
yul <aymeric.guth@protonmail.com>
2026-05-02 12:18:57 +0200

    add 4th path to mplayer-layout

---
+++ scripts/mplayer-layout.lua
@@ -0,0 +1,4 @@
+vim.cmd('vsplit /home/yul/Downloads')
+vim.cmd('split /home/yul/Music/playlists')
+vim.cmd('split /home/yul/Music/music')
+vim.cmd('execute "normal \\<C-w>l"')
--- scripts/mplayer-remove-track.sh
+++ scripts/mplayer-remove-track.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-/usr/bin/kitty --app-id e2696752-512f-11f0-ae75-ab75b60c01aa  /usr/bin/nvim "/home/yul/Music" \
-    "+vsplit | split" \
-    "+lua vim.wait(50, function() return false end)" \
-    '+execute "normal 6j\<CR>"' \
-    "+lua vim.wait(10, function() return false end)" \
-    '+execute "normal \<C-w>j8j\<CR>"' \
-    "+lua vim.wait(10, function() return false end)" \
-    '+execute "normal \<C-w>l7j\<CR>"'
+# /usr/bin/kitty --app-id e2696752-512f-11f0-ae75-ab75b60c01aa  /usr/bin/nvim "/home/yul/Music" \
+#     "+vsplit | split" \
+#     "+lua vim.wait(50, function() return false end)" \
+#     '+execute "normal 6j\<CR>"' \
+#     "+lua vim.wait(10, function() return false end)" \
+#     '+execute "normal \<C-w>j8j\<CR>"' \
+#     "+lua vim.wait(10, function() return false end)" \
+#     '+execute "normal \<C-w>l7j\<CR>"'
+/usr/bin/kitty --app-id e2696752-512f-11f0-ae75-ab75b60c01aa /usr/bin/nvim "/home/yul/Music/music.inbox" \
+  '+luafile ~/dev/personal/dotfiles/scripts/mplayer-layout.lua'
 systemd-run --user ~/dev/personal/dotfiles/scripts/mplayer-sync.sh
