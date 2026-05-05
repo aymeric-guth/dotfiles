7f85b5446f84f890ebc0aef41cc25e30ee436eea
yul <aymeric.guth@protonmail.com>
2026-05-05 05:30:46 +0200

    add cypher to backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -8,6 +8,7 @@ fi
 cp /home/yul/.local/share/zsh/.zsh_history "/home/yul/Backup/zsh/$(date '+%Y-%m-%d')_zsh_history"
 cp /home/yul/.config/BraveSoftware/Brave-Browser/Default/History "/home/yul/Backup/Brave/$(date '+%Y-%m-%d')_History"
 cp /home/yul/.config/BraveSoftware/Brave-Browser/Default/Bookmarks "/home/yul/Backup/Brave/$(date '+%Y-%m-%d')_Bookmarks"
+7z a -p"$(cat ~/.archive_pass)" -mhe=on /home/yul/Backup/ssh/$(date "+%Y-%m-%d")_ssh.7z /home/yul/.ssh
 
 cd /home/yul/Music
 find music.inbox | sort > "/home/yul/Backup/Music/$(date '+%Y-%m-%d')_music.inbox"
