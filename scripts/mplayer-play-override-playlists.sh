be67cc62889567d8cc1bcb73028548bf1bdbac21
yul <aymeric.guth@protonmail.com>
2026-08-02 12:42:13 +0200

    feat/oil-copy

--- scripts/mplayer-play-override-playlists.sh
+++ scripts/mplayer-play-override-playlists.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-file="$(find /home/yul/Music/playlists/ -maxdepth 2 -mindepth 2 | sed "s|^/home/yul/Music/||" | sort | rofi -p "play" -dmenu -i -l 10 -config $DOTFILES/rofi/mplayer.rasi)" || exit 0
+file="$(find /home/yul/Music/playlists/ -maxdepth 1 | sed "s|^/home/yul/Music/||" | sort | rofi -p "play" -dmenu -i -l 10 -config $DOTFILES/rofi/mplayer.rasi)" || exit 0
 if [ -z $file ]; then
   return 1;
 fi
@@ -8,11 +8,9 @@ fi
 mpc stop
 mpc clear
 
-notify-send "file: ${file}"
 if [ -f /home/yul/Music/"$file" ]; then
-  playlist="labels/$(basename -s .m3u "$file")"
+  playlist="$(basename -s .m3u "$file")"
   mpc load "$playlist"
-  notify-send "playlist: ${playlist}"
 else
   mpc listall "$file" | sort -V | while IFS= read -r f; do
     mpc add "$f"
